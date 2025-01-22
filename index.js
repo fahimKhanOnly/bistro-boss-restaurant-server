ceacd147c85587f6fe56d711db03df1fd4d7be3a
fahimKhanOnly <fahimmontacir767@gmail.com>
2025-01-22 14:34:33 +0600

    Create post API for get user Carted food.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 
 
@@ -25,14 +26,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const bistroDBCollections = client.db("bistroDB").collection("menu");
+    const bistroDBCollections = client.db("bistroDB")
+    const bistroMenu = bistroDBCollections.collection("menu");
+    const cartCollections = bistroDBCollections.collection("carts");
 
 
     app.get('/menu', async(req, res) => {
-      const result = await bistroDBCollections.find().toArray();
+      const result = await bistroMenu.find().toArray();
       res.send(result);
     })
-    
+
+    app.post('/carts', async (req, res) => {
+      const data = req.body;
+      const result = await cartCollections.insertOne(data);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
@@ -43,8 +52,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
 
 
 
