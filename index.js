8d22176cf5feadcd814fc9afecb4c1e0f4ef0b63
fahimKhanOnly <fahimmontacir767@gmail.com>
2025-01-24 09:24:20 +0600

    Create get API for fetch cart items from client side.

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollections.find(query).toArray();
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
