565618f854fcdc33f2f233b2d614617511e96f72
fahimKhanOnly <fahimmontacir767@gmail.com>
2025-02-17 14:27:47 +0600

    Create post api for add new item from admin dashboard.

--- index.js
+++ index.js
@@ -33,7 +33,57 @@ async function run() {
     const userCollections = bistroDBCollections.collection("users");
 
 
-    app.get('/menu', async(req, res) => {
+
+    // JWT APIs
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_TOKEN, { expiresIn: '3h' });
+      res.send({ token });
+    })
+
+    const verifyToken = (req, res, next) => {
+      // console.log(req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'Unauthorized Access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // Use verify token after 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollections.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'Forbidden Access'});
+      }
+      next();
+    }
+
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'Forbidden Access' })
+      }
+      const query = {email: email};
+      const user = await userCollections.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user.role == 'admin';
+      }
+      res.send({admin});
+    })
+
+    app.get('/menu', async (req, res) => {
       const result = await bistroMenu.find().toArray();
       res.send(result);
     })
@@ -46,14 +96,14 @@ async function run() {
 
     app.get('/carts', async (req, res) => {
       const email = req.query.email;
-      const query = {email: email};
+      const query = { email: email };
       const result = await cartCollections.find(query).toArray();
       res.send(result);
     })
 
     app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollections.deleteOne(query);
       res.send(result);
     })
@@ -63,36 +113,42 @@ async function run() {
     // Users related APIs
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await userCollections.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
       const result = await userCollections.insertOne(user);
       res.send(result);
     })
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollections.find().toArray();
       res.send(result);
     })
 
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollections.deleteOne(query);
       res.send(result);
     })
 
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedData = {
         $set: {
           role: 'admin',
         }
       };
       const result = await userCollections.updateOne(filter, updatedData);
+      res.send(result);
+    })
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const data = req.body;
+      const result = await bistroMenu.insertOne(data);
       res.send(result);
     })
 
